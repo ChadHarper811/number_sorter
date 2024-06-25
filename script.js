88f96b095d2ad2f33e11fd44a503fca3c55293ac
Chad Harper <chadharper811@gmail.com>
2024-06-24 21:12:20 -0500

    added new function to update ui with the sorted numbers

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+};
+
+const updateUI = (array = []) => {
+    
 }
 
 sortButton.addEventListener("click", sortInputArray);
