31feff1a6cf53656991f3e5e11f72bcc6aeb7bf3
Chad Harper <chadharper811@gmail.com>
2024-06-25 18:24:15 -0500

    finished insertion sorting function

--- script.js
+++ script.js
@@ -47,12 +47,14 @@ const selectionSort = (array) => {
 const insertionSort = (array) => {
     for (let i = 1; i< array.length; i++) {
         const currValue = array[i];
-        let j = i-1;
+        let j = i - 1;
         while (j >= 0 && array[j] > currValue) {
             array[j + 1] = array[j];
             j--;
         }
+        array[j + 1] = currValue;
     }
-}
+    return array;
+};
 
 sortButton.addEventListener("click", sortInputArray);
