e6cefdb640b2256daf1a3700ce7268249deec7d5
Chad Harper <chadharper811@gmail.com>
2024-06-24 21:31:22 -0500

    finished bubble sort function

--- script.js
+++ script.js
@@ -3,14 +3,34 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
-    updateUI(inputValues)
+
+    const sortedValues = bubbleSort(inputValues);
+
+    updateUI(sortedValues);
 };
 
 const updateUI = (array = []) => {
     array.forEach((num, i) => {
         const outputValueNode = document.getElementById(`output-value-${i}`);
         outputValueNode.innerText = num
-    })
+    });
+};
+
+const bubbleSort = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j+1]) {
+                const temp = array[j] 
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+    return array;
+}
+
+const selectionSort = (array) => {
+    
 }
 
 sortButton.addEventListener("click", sortInputArray);
