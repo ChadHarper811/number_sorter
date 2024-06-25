1deafedd0e763a86354bd76fdeb630f93f1606dd
Chad Harper <chadharper811@gmail.com>
2024-06-25 18:16:56 -0500

    added loop to function

--- script.js
+++ script.js
@@ -45,7 +45,14 @@ const selectionSort = (array) => {
 };
 
 const insertionSort = (array) => {
-
+    for (let i = 1; i< array.length; i++) {
+        const currValue = array[i];
+        let j = i-1;
+        while (j >= 0 && array[j] > currValue) {
+            array[j + 1] = array[j];
+            j--;
+        }
+    }
 }
 
 sortButton.addEventListener("click", sortInputArray);
