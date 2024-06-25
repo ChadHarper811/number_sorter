10835fee7cf1efbd60fd3ebd3c043462c97e8961
Chad Harper <chadharper811@gmail.com>
2024-06-24 21:07:11 -0500

    added input values from drowpdowns into function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
 }
 
 sortButton.addEventListener("click", sortInputArray);
