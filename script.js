52352ff76c71d9d96671a06d9544c9770b722f65
Chad Harper <chadharper811@gmail.com>
2024-06-24 21:17:59 -0500

    finished functionality on updateUI

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
     event.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
+    updateUI(inputValues)
 };
 
 const updateUI = (array = []) => {
-    
+    array.forEach((num, i) => {
+        const outputValueNode = document.getElementById(`output-value-${i}`);
+        outputValueNode.innerText = num
+    })
 }
 
 sortButton.addEventListener("click", sortInputArray);
